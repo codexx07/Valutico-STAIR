ab6857a846c08cd3fa2a1c7d205bc3db46400670
codexx07 <sparsh.riju@gmail.com>
2023-11-14 02:19:24 +0530

    Skeleton of Home page

--- Valutico-Stair/components/NavBar.jsx
+++ Valutico-Stair/components/NavBar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import './NavBar.css';
 
-function NavBar() {
-  // Add your navigation items here
+// Import icons if you have them as SVGs or components
+// import Logo from './path-to-your-logo.svg';
+// import AddIcon from './path-to-add-icon.svg';
+// ...
+
+const NavBar = () => {
   return (
-    <div className="NavBar">
-      {/* Render your navigation items here */}
+    <div className="sidebar">
+      <div className="logo">
+        {/* Replace "Logo" with your actual logo component or <img> tag */}
+        {/* <Logo /> */}
+        STAIR
+      </div>
+      <div className="menu">
+        {/* Replace these with actual icons/components and add onClick handlers as needed */}
+        <div className="menu-item">
+          {/* <AddIcon /> */}
+          Add Files...
+        </div>
+        <div className="menu-item">
+          {/* <CompareIcon /> */}
+          Compare...
+        </div>
+        {/* ...other menu items */}
+        <div className="menu-item">Help</div>
+        <div className="menu-item">Info</div>
+        <div className="menu-item">Profile</div>
+      </div>
     </div>
   );
-}
+};
 
 export default NavBar;
