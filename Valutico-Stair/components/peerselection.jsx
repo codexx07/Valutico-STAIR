a51704e958d3da78f52b32ce8a3f410c10ac8ccb
codexx07 <sparsh.riju@gmail.com>
2023-11-16 02:25:06 +0530

    Peer Table Implementation 1

--- Valutico-Stair/components/peerselection.jsx
+++ Valutico-Stair/components/peerselection.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import './PeerSelection.css';
-import searchIcon from '../src/assets/Search.png'; // Update this path to the actual location of your image file
+import searchIcon from '../src/assets/Search.png';
 
 const PeerSelection = () => {
-  // Define your search handler here
+  const [activeTab, setActiveTab] = useState('trading');
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    // Replace 'http://localhost:8000/api/your-endpoint' with the actual endpoint from where you're fetching data
+    fetch('http://localhost:8000/api/your-endpoint')
+      .then(response => response.json())
+      .then(data => setTableData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); // The empty array causes this effect to only run on mount
+
   const handleSearch = () => {
     console.log('Search initiated');
     // Implement your search logic here
@@ -11,6 +22,8 @@ const PeerSelection = () => {
 
   return (
     <div className="peerSelection">
+      {/* ...other components */}
+
       <div className="peerSelectionHeader">
         <h1>Peer Selection</h1>
         <div className="valuationInfo">
@@ -29,13 +42,67 @@ const PeerSelection = () => {
         </div>
       </div>
       <div className="tabMenu">
-        <button className="tabButton">Trading</button>
-        <button className="tabButton">Benchmarking</button>
-        <button className="tabButton">Beta</button>
+        <button onClick={() => setActiveTab('trading')} className={`tabButton ${activeTab === 'trading' ? 'active' : ''}`}>Trading</button>
+        <button onClick={() => setActiveTab('benchmarking')} className={`tabButton ${activeTab === 'benchmarking' ? 'active' : ''}`}>Benchmarking</button>
+        <button onClick={() => setActiveTab('beta')} className={`tabButton ${activeTab === 'beta' ? 'active' : ''}`}>Beta</button>
       </div>
-      {/* Include the table and other content components here */}
+      {activeTab === 'trading' && (
+        <div className="tableContainer">
+         <table>
+            <thead>
+                <tr>
+                <th rowSpan="2">Name</th>
+                <th colSpan="2">Company Info</th>
+                <th colSpan="3">EV/Sales</th>
+                <th colSpan="3">EV/EBITDA</th>
+                <th colSpan="2">EV/EBIT</th>
+                {/* Adjust the colSpan values according to the number of subheadings */}
+                </tr>
+                <tr>
+                {/* These are the subheadings under "Company Info" */}
+                <th>Country</th>
+                <th>Market Cap INR</th>
+                
+                {/* These are the subheadings under "EV/Sales" */}
+                <th>2021</th>
+                <th>2022</th>
+                <th>2023</th>
+                
+                {/* Add subheadings for EV/EBITDA */}
+                <th>2021</th>
+                <th>2022</th>
+                <th>2023</th>
+                
+                {/* Add subheadings for EV/EBIT */}
+                <th>2021</th>
+                <th>2022</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableData.map(item => (
+                <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.country}</td>
+                    <td>{item.marketCap}</td>
+                    {/* Output additional data as needed */}
+                    <td>{item.evSales2021}</td>
+                    <td>{item.evSales2022}</td>
+                    <td>{item.evSales2023}</td>
+                    <td>{item.evEbitda2021}</td>
+                    <td>{item.evEbitda2022}</td>
+                    <td>{item.evEbitda2023}</td>
+                    <td>{item.evEbit2021}</td>
+                    <td>{item.evEbit2022}</td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+
+        </div>
+      )}
     </div>
   );
 };
 
 export default PeerSelection;
+
