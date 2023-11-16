5f64cd373c22bd12c9d65f64432d6885f9761a4d
codexx07 <sparsh.riju@gmail.com>
2023-11-17 02:32:11 +0530

    QA housekeeping

--- Valutico-Stair/components/AssessmentChart.jsx
+++ Valutico-Stair/components/AssessmentChart.jsx
@@ -29,6 +29,19 @@ const AssessmentChart = () => {
     'Exposure To Business Cycle': 50,
     'Exposure To Political Legal Factors': 50,
     'Management Quality': 50,
+    'Reliance On Key Persons':50,
+    'Reliance On Key Customers':50,
+    'Reliance On Distribution Partners':50,
+    'Customer Lock in':50,
+    'Input Cost Risk':50,
+    'Reliance On Distribution Partners':50,
+    'Product Quality':50,
+    'Innovativeness':50,
+    'Intellectual Property Protection':50,
+    'Risk of Technological Disruption':50,
+    'Capital Intensity':50,
+    'Leverage':50,
+    'Foreign Exchange/Currency Risk':50
   });
 
   const handleSliderChange = (criteria, newValue) => {
@@ -43,8 +56,12 @@ const AssessmentChart = () => {
   const averageValue = calculateAverage();
 
   const generalSliders = ['Company Size', 'Positioning'];
-  const marketSliders = ['Market Size', 'Market Growth', 'Market Share'];
-  const managementSliders = ['Competition', 'Exposure To Business Cycle', 'Exposure To Political Legal Factors', 'Management Quality'];
+  const marketSliders = ['Market Size', 'Market Growth', 'Market Share','Competition', 'Exposure To Business Cycle', 'Exposure To Political Legal Factors'];
+  const managementSliders = ['Management Quality','Reliance On Key Persons'];
+  const salessliders =['Reliance On Key Customers','Reliance On Distribution Partners', 'Customer Lock In'];
+  const purchasingSliders =['Input Cost Risk', 'Reliance On Distribution Partners'];
+  const productSliders =['Product Quality', 'Innovativeness','Intellectual Property Protection','Rsik Of Technological Disruption'];
+  const FinancialSliders = ['Capital intensity','Leverage','Foreign Exchange/Currency Risk'];
 
   return (
     <div className="assessment-chart">
@@ -75,6 +92,42 @@ const AssessmentChart = () => {
       ))}
       <div className="section-heading">Management</div>
       {managementSliders.map((criteria) => (
+        <AssessmentSlider
+          key={criteria}
+          label={criteria}
+          value={values[criteria]}
+          onChange={handleSliderChange}
+        />
+      ))}
+      <div className="section-heading">Sales</div>
+      {salessliders.map((criteria) => (
+        <AssessmentSlider
+          key={criteria}
+          label={criteria}
+          value={values[criteria]}
+          onChange={handleSliderChange}
+        />
+      ))}
+      <div className="section-heading">Purchasing</div>
+      {purchasingSliders.map((criteria) => (
+        <AssessmentSlider
+          key={criteria}
+          label={criteria}
+          value={values[criteria]}
+          onChange={handleSliderChange}
+        />
+      ))}
+      <div className="section-heading">Product</div>
+      {productSliders.map((criteria) => (
+        <AssessmentSlider
+          key={criteria}
+          label={criteria}
+          value={values[criteria]}
+          onChange={handleSliderChange}
+        />
+      ))}
+      <div className="section-heading">Financial</div>
+      {FinancialSliders.map((criteria) => (
         <AssessmentSlider
           key={criteria}
           label={criteria}
