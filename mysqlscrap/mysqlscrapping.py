a13252922b57c04883603e7d14c9cda0d2aaa9bc
Tanmay Dwivedi <tanmay.dwivedi2022@vitstudent.ac.in>
2023-11-17 15:03:45 +0530

    tatadata

--- mysqlscrap/mysqlscrapping.py
+++ mysqlscrap/mysqlscrapping.py
@@ -1,77 +1,46 @@
-import re
+import pandas as pd
+from sqlalchemy import create_engine
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import TimeoutException
-import psycopg2  # Import the psycopg2 library for PostgreSQL
-from psycopg2 import Error
 
-TIMEOUT_VALUE = 10
+# Scrape the data using Selenium
+driver = webdriver.Chrome()
+driver.get('https://www.bseindia.com/stock-share-price/tata-motors-ltd/tatamotors/500570/financials-results/')
+xpath = '//*[@id="qtly"]/table/tbody/tr/td/table[1]'
+table = driver.find_element(By.XPATH, xpath)
 
-def convert_financial_string(s):
-    try:
-        match = re.search(r'-?\d+,\d+\.?\d*|-?\d+\.?\d*', s.replace(',', ''))
-        return float(match.group()) if match else None
-    except ValueError:
-        return None
+data = []
+for row in table.find_elements(By.XPATH, './/tr'):
+    row_data = [cell.text for cell in row.find_elements(By.XPATH, './/td')]
+    if row_data:
+        data.append(row_data)
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+driver.quit()
 
-# Initialize connection with None
-connection = None
+# Assuming the first row of scraped data is the header
+headers = data.pop(0)
+df = pd.DataFrame(data, columns=headers)
 
-try:
-    driver.get("https://www.bseindia.com/stock-share-price/tata-motors-ltd/tatamotors/500570/financials-results/")
+# Save the data to a CSV file
+csv_file_path = 'table_data.csv'
+df.to_csv(csv_file_path, index=False)
 
-    headers = WebDriverWait(driver, TIMEOUT_VALUE).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table thead th"))
-    )
-    header_titles = [header.text.lower().replace(' ', '_') for header in headers]
+# Database connection parameters
+db_username = 'postgres'
+db_password = '12345'
+db_name = 'postgres'
+db_host = 'localhost'
 
-    rows = WebDriverWait(driver, TIMEOUT_VALUE).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "tbody tr"))
-    )
+# Creating a SQLAlchemy engine
+engine = create_engine(f'postgresql+psycopg2://{db_username}:{db_password}@{db_host}/{db_name}')
 
-    # Establish a PostgreSQL connection
-    connection = psycopg2.connect(
-        host='localhost',
-        database='bse_financials',
-        user='user',
-        password='12345'
-    )
-    cursor = connection.cursor()
+# Reading the CSV file
+df = pd.read_csv(csv_file_path)
 
-    for row in rows:
-        cells = row.find_elements(By.CSS_SELECTOR, "td")
-        if cells:
-            financial_data = [convert_financial_string(cell.text) for cell in cells]
+# Infer the data types for SQL Alchemy
+df.to_sql('financial_data', engine, if_exists='replace', index=False, method='multi')
 
-            if len(financial_data) != len(header_titles):
-                print(f"Skipping row due to column count mismatch: {financial_data}")
-                continue
+print("Table created and data inserted into PostgreSQL database.")
 
-            insert_query = f"INSERT INTO company_financials ({', '.join(header_titles)}) VALUES ({', '.join(['%s'] * len(financial_data))})"
-            cursor.execute(insert_query, tuple(financial_data))
 
-    connection.commit()
-
-except TimeoutException:
-    print("Timed out waiting for page to load. Consider increasing the timeout value or checking your CSS selectors.")
-    driver.save_screenshot('timeout_screenshot.png')
-    print("Current URL:", driver.current_url)
-    print("Page title:", driver.title)
-except Error as e:
-    print("PostgreSQL Error: ", e)
-except Exception as e:
-    print("General Error: ", e)
-finally:
-    driver.quit()
-    if connection:
-        cursor.close()
-        connection.close()
 
